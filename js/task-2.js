969c5484de88d150e1cf8ce5d3e1113c1d22bfda
sonja <signtieva@gmail.com>
2024-09-28 16:25:39 +0300

    add js

---
+++ js/task-2.js
@@ -0,0 +1,36 @@
+'use strict'
+
+class Storage {
+    #items
+    constructor(items) {
+        this.items = items
+    }
+    getItems() {
+        return this.items;
+    }
+    addItem(newItem) {
+        this.items.push(newItem);
+    }
+    removeItem(itemToRemove) {
+        if (this.items.includes(itemToRemove)) {
+            let i = this.items.indexOf(itemToRemove)
+            this.items.splice(1, i);
+        }
+        else {
+            return this.items;
+        }
+
+    }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
