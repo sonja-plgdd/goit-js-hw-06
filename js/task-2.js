8e3222293b5f7d5356b9b3283e09bcb091477603
sonja <signtieva@gmail.com>
2024-09-29 14:57:00 +0300

    fix task-2

--- js/task-2.js
+++ js/task-2.js
@@ -3,23 +3,19 @@
 class Storage {
     #items
     constructor(items) {
-        this.items = items
+        this.#items = items
     }
     getItems() {
-        return this.items;
+        return this.#items;
     }
     addItem(newItem) {
-        this.items.push(newItem);
+        this.#items.push(newItem);
     }
     removeItem(itemToRemove) {
-        if (this.items.includes(itemToRemove)) {
-            let i = this.items.indexOf(itemToRemove)
-            this.items.splice(1, i);
+        if (this.#items.includes(itemToRemove)) {
+            let i = this.#items.indexOf(itemToRemove)
+            this.#items.splice(i, 1);
         }
-        else {
-            return this.items;
-        }
-
     }
 }
 
